a2d052d2a393109ef8a8171e4f830d8337241d52
Christina Nguyen <phuonghien716@gmail.com>
2016-04-18 01:21:15 -0400

    init commit

---
+++ app/src/main/java/io/tina/chris/meshaging/MainActivity.java
@@ -0,0 +1,37 @@
+package io.tina.chris.meshaging;
+
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.nio.channels.Channel;
+
+public class MainActivity extends AppCompatActivity {
+    // Initializing variable(s)
+    private final IntentFilter intentFilter = new IntentFilter();
+    WifiP2pManager.Channel mChannel;
+    WifiP2pManager mManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Indicates a change in the Wi-Fi P2P status.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        // Indicates a change in the list of available peers.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        // Indicates the state of Wi-Fi P2P connectivity has changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        // Indicates this device's details have changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        // Gets an instance of the WifiP2pManager and calls its initialize() method.
+        // The method returns a WifiP2pManager.Channel object, which is used later to
+        // connect the app to thw Wi-Fi P2P framework.
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+    }
+}
